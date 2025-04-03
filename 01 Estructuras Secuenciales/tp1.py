44908eea7ed34fba2cd7ab5d334d313f3a936c28
naza2910 <nazareno.romero2910@gmail.com>
2025-04-03 00:22:34 -0300

    tp1 y tp2 subidos

---
+++ 01 Estructuras Secuenciales/tp1.py
@@ -0,0 +1,66 @@
+# actividad 1
+'''
+print("Hola Mundo!")
+'''
+# actividad 2
+'''
+nombre = input("ingrese su nombre")
+print(f"Hola {nombre}")
+'''
+#actividad 3
+'''
+nombre = input("ingrese su nombre")
+edad = input(f"{nombre} ingresa tu edad")
+pais = input(f"{nombre} ingrese su pais de residecia")
+print(f"soy {nombre} tengo {edad} años y resido en {pais}")
+'''
+# actividad 4
+'''
+radio = float("ingrese el radio del circulo")
+area = float(f"3.14159265359 * {radio} ** 2"  )
+perimetro = float(f"2 * {radio} * 3.14159265359")
+print(f"el area es = {area} y el perimetro = {perimetro}")
+'''
+# actividad 5
+'''
+segundos = int("ingrese la cantidad de segundos que quiera pasar a horas")
+print(f"{segundos} segundos equivale a {segundos / 3600} horas")
+'''
+# actividad 6
+'''
+num = float("ingrese su numero")
+print(f"las tablas son x1 {num * 1}, x2 {num * 2}, x3 {num * 3}, x4 {num * 4}, x5 {num * 5}, x6 {num * 6} ,x7 {num * 7} ,x8 {num * 8} ,x9 {num * 9},x10 {num * 10}, ")
+'''
+# actividad 7
+'''
+num1 = int("ingrese su primer numero entero")
+num2 = int("ingrese su segundo numero entero")
+suma = num1 +num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+print(f"la suma = {num1} + {num2} = {suma}")
+print(f"la resta = {num1} - {num2} = {resta}")
+print(f"la multiplicacion = {num1} * {num2} = {multiplicacion}")
+print(f"la division = {num1} / {num2} = {division}")
+'''
+#actividad 8
+altura = float("ingrese su altura en metros, EJEMPLO: 1,80")
+peso = float("ingrese su peso en kg, ejemplo: 80,5|70")
+IMC = peso / (altura) **2
+print(f"su masa corporal es de: {IMC} ")
+
+# actividad 9
+'''
+celcius = float("ingrese temperatura en grados celcius")
+grados_Fahrenheit = (9 / 5 * celcius) + 32
+print(f"su equivalente en grados Fahrenheit es: {grados_Fahrenheit}")
+'''
+# actividad 10
+'''
+num1 = float("ingrese su primer numero")
+num2 = float("ingrese su segundo numero")
+num3 = float("ingrese su tercer numero")
+promedio = (num1 + num2 + num3) / 3
+print(f"el promedio de los 3 numeros es:{promedio} ")
+'''
