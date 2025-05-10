2c0a26d1c380d1d0339eccc781517b619764b93f
naza2910 <nazareno.romero2910@gmail.com>
2025-05-09 23:55:04 -0300

    tp listas

---
+++ 06 Datos complejos/tp_listas.py
@@ -0,0 +1,29 @@
+##actividad 1
+'''
+multiplos_de_4 = list(range(4, 101, 4))
+print(multiplos_de_4)
+'''
+##actividad 2
+'''
+elementos = ["mouse", "teclado", "celular", "maquina", "codigo"]
+print(elementos[-3])
+'''
+##actividad 3
+'''
+lista_vacia = []
+lista_vacia.append("codigo")
+lista_vacia.append("birome")
+lista_vacia.append("perro")
+print(lista_vacia)
+'''
+#actividad 4
+'''
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales)
+'''
+##actividad 5
+numeros = [8, 15, 3, 22, 7]
+numeros.remove(max(numeros))
+print(numeros)
